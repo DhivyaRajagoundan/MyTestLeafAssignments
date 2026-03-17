31e2c27ec621fd6364376f6ee0a9a2a20856d78a
DhivyaRajagoundan <66466992+DhivyaRajagoundan@users.noreply.github.com>
2026-03-17 15:35:49 +0530

    Add files via upload

---
+++ dayOneHomeAssignment.js
@@ -0,0 +1,46 @@
+//DAY 01 - DATA TYPES
+//Declare variable using let keyword.
+
+//1.NUMBER
+let contactNumber = 9944310797;
+console.log("The Contact Number is :" , contactNumber);
+console.log(typeof contactNumber);
+
+//2.STRING
+let userName = "Dhivya";
+let organization= 'Autodesk';
+let userName3 = `Dhivya Rajagoudan`;
+console.log("The User Name is :", userName);
+console.log(typeof userName);
+console.log("The User Organization is :",organization);
+console.log(typeof organization);
+console.log("The User Name is :",userName3)
+console.log(typeof userName3);
+
+//3.BOOLEAN
+ let userKnowsAutomation = true;
+ let userDontKnowAutomation = false;
+ console.log("Whether John Knows Automation ?:" , userKnowsAutomation);
+ console.log(typeof userKnowsAutomation);
+ console.log("Whether Smith Knows Automation", userDontKnowAutomation);
+ console.log(typeof userDontKnowAutomation);
+
+ //4.UNDEFINED
+
+ let usesPlaywright;
+ console.log("Current Automation Tool in Trend",usesPlaywright);
+ console.log(typeof usesPlaywright);
+
+ //5.NULL
+
+ let middleName = null;
+ console.log("The User Middle Name is:",middleName);
+ console.log(typeof middleName);
+
+ //6.BIG INT
+
+ let accountNumber = 9999923232312323322n;
+ console.log("The Account Number is:",accountNumber);
+ console.log(typeof accountNumber);
+
+
