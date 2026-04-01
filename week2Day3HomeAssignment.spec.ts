1af61126bf63edd160840ea350288ce4d61951ed
DhivyaRajagoundan <66466992+DhivyaRajagoundan@users.noreply.github.com>
2026-04-01 16:05:54 +0530

    Update week2Day3HomeAssignment.spec.ts

--- week2Day3HomeAssignment.spec.ts
+++ week2Day3HomeAssignment.spec.ts
@@ -1,29 +1,29 @@
 // Assignment Two Edit Leads
 
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 
-test('Week2 Day3 Assignment 2 Edit Lead Field',async({page})=>{
-    
-await page.goto("http://leaftaps.com/opentaps/control/main");
-await page.locator('//input[@name="USERNAME"]').fill("Demosalesmanager");
-await page.locator('//input[contains(@id,"password")]').fill("crmsfa");
-await page.click('//input[@class="decorativeSubmit"]');
-await page.click('//a[contains(text(),"CRM")]');
-await page.click('a:has-text("Leads")');
-await page.click('//a[contains(text(),"Create Lead")]');
-await page.locator('//input[@id="createLeadForm_companyName"]').fill("Logistics Pvt Ltd");
-await page.locator('(//input[@name="firstName"])[3]').fill("Dhivya");
-await page.locator('(//input[@name="lastName"])[3]').fill("Rajagoundan");
-await page.click('//input[@class="smallSubmit"]');
-await page.click('a:has-text("Edit")');
-await page.locator('//input[@id="updateLeadForm_companyName"]').fill("Updated Company Name");
-await page.click('//input[@value="Update"]');
+test('Week2 Day3 Assignment 2 Edit Lead Field', async ({ page }) => {
+
+    await page.goto("http://leaftaps.com/opentaps/control/main");
+    await page.locator('//input[@name="USERNAME"]').fill("Demosalesmanager");
+    await page.locator('//input[contains(@id,"password")]').fill("crmsfa");
+    await page.click('//input[@class="decorativeSubmit"]');
+    await page.click('//a[contains(text(),"CRM")]');
+    await page.click('a:has-text("Leads")');
+    await page.click('//a[contains(text(),"Create Lead")]');
+    await page.locator('//input[@id="createLeadForm_companyName"]').fill("Logistics Pvt Ltd");
+    await page.locator('(//input[@name="firstName"])[3]').fill("Dhivya");
+    await page.locator('(//input[@name="lastName"])[3]').fill("Rajagoundan");
+    await page.click('//input[@class="smallSubmit"]');
+    await page.click('a:has-text("Edit")');
+    await page.locator('//input[@id="updateLeadForm_companyName"]').fill("Updated Company Name");
+    await page.click('//input[@value="Update"]');
 })
- 
 
 
-    test('Assignment 1 Create Lead', async ({ page }) => {
+
+test('Assignment 1 Create Lead', async ({ page }) => {
     await page.goto("https://login.salesforce.com/");
     await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com')
     await page.locator('input[id="password"]').fill('TestLeaf@2025');
@@ -46,54 +46,54 @@ await page.click('//input[@value="Update"]');
     //const hasText = await page.locator(`text='${leadName}'`).innerText();
     const hasText = await page.locator(`//div[@title,'${leadName}']`).innerText();
     //a[contains(@title,'Test Lead')]
-     if(hasText===`${leadName}`){
+    if (hasText === `${leadName}`) {
         console.log("Lead Text is visible");
     }
-    else{
+    else {
         console.log("Lead Text is not visible");
-        
-    }   
-}) 
- 
+
+    }
+})
 
 
-test('Week 2 Day 3 Assignment 3',async({page})=>{
-await page.goto("https://login.salesforce.com/");
-await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com');
-await page.locator('input[id="password"]').fill('TestLeaf@2025');
-await page.locator('#Login').click();
-await page.waitForTimeout(12000);
-await page.click('//button[@title="App Launcher"]');
-await page.click('//button[@aria-label="View All Applications"]');
-await page.click('//p[text()="Individuals"]');
-await page.click('//button[@title="Select a List View: Individuals"]');
-await page.click('//div[@title="New"]');
-const lastName = "lastnametest";
-await page.locator('//input[@placeholder="Last Name"]').fill(`${lastName}`);
-await page.click('//span[text()="Save"]');
-/*const text = await page.locator(`//div[@title='${lastName}']`).innerText();
-console.log(text)
-if(text===`${lastName}`){
-        console.log("Individual is created");
 
-}else{
-    console.log("Individual is not created");
-}  */
-}) 
+test('Week 2 Day 3 Assignment 3', async ({ page }) => {
+    await page.goto("https://login.salesforce.com/");
+    await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com');
+    await page.locator('input[id="password"]').fill('TestLeaf@2025');
+    await page.locator('#Login').click();
+    await page.waitForTimeout(12000);
+    await page.click('//button[@title="App Launcher"]');
+    await page.click('//button[@aria-label="View All Applications"]');
+    await page.click('//p[text()="Individuals"]');
+    await page.click('//button[@title="Select a List View: Individuals"]');
+    await page.click('//div[@title="New"]');
+    const lastName = "lastnametest";
+    await page.locator('//input[@placeholder="Last Name"]').fill(`${lastName}`);
+    await page.click('//span[text()="Save"]');
+    await expect(page.locator(`//div[@title='${lastName}']`)).toContainText(`${lastName}`);
+})
 
- test('Week 2 Day 3 Edit Individual Fields',async({page})=>{
-await page.goto("https://login.salesforce.com/");
-await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com');
-await page.locator('input[id="password"]').fill('TestLeaf@2025');
-await page.locator('#Login').click();
-await page.waitForTimeout(12000);
-await page.click('//span[text()="More"]');
-await page.click('(//span[text()="Individuals"])[2]')
-await page.click('//div[@title="New"]');
-const lastName = "lastnametest";
-await page.locator('//input[@placeholder="Last Name"]').fill(`${lastName}`);
-await page.click('//span[text()="Save"]');
-/// have to continue after learning search
+test('Week 2 Day 3 Edit Individual Fields', async ({ page }) => {
+    await page.goto("https://login.salesforce.com/");
+    await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com');
+    await page.locator('input[id="password"]').fill('TestLeaf@2025');
+    await page.locator('#Login').click();
+    await page.waitForTimeout(12000);
+    await page.click('//span[text()="More"]');
+    await page.click('(//span[text()="Individuals"])[2]')
+    await page.click('//div[@title="New"]');
+    const lastName = "EditIndivi";
+    await page.locator('//input[@placeholder="Last Name"]').fill(`${lastName}`);
+    await page.click('//span[text()="Save"]');
+    await page.click('//a[@title="Individuals"]');
+    await page.click(`//a[@title='${lastName}']`);
+    await page.getByRole('button', { name: 'Edit', exact: true }).click();
+    await page.getByRole('button', { name: 'Salutation' }).click();
+    await page.click('//a[@title="Mr."]');
+    const firstName = "Dhivya"
+    await page.getByPlaceholder("First Name").fill(`${firstName}`);
+    await page.click('//span[text()="Save"]');
+    await expect(page.locator('//div[text()="Individual"]/following-sibling::div/span')).toContainText(`${firstName}`);
 
 })
- 
